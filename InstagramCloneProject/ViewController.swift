18e2b34ea2a1f05d5fc632fd7463773dbc8e2be7
Halil <halilibrahimbaykan@gmail.com>
2020-07-19 03:02:52 +0200

    add Firebase

--- InstagramCloneProject/ViewController.swift
+++ InstagramCloneProject/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -15,52 +16,97 @@ class ViewController: UIViewController {
         btn.setImage(#imageLiteral(resourceName: "addImage"), for: UIControl.State.normal)
         
         //        btn.backgroundColor = .yellow
-        btn.translatesAutoresizingMaskIntoConstraints = false // with this it accept the constraints for autolayout
+        btn.translatesAutoresizingMaskIntoConstraints = false // with this button accepts the constraints for autolayout
         return btn
     }()
-
+    
     let txtEmail: UITextField = {
         let txt = UITextField()
-        txt.translatesAutoresizingMaskIntoConstraints = false
+        //        txt.translatesAutoresizingMaskIntoConstraints = false // we don't need this anymore because we added inside of the anchor func
         txt.placeholder = "Enter your email.."
         txt.backgroundColor = UIColor(white: 0, alpha: 0.05)
         txt.borderStyle = .roundedRect
         txt.font = UIFont.systemFont(ofSize: 16)
+        txt.addTarget(self, action: #selector(valueChanging), for: .editingChanged)
         return txt
     }()
     
+    @objc fileprivate func valueChanging(){
+        let checkFormCharacter =
+            (txtEmail.text?.count ?? 0) > 0 &&
+            (txtUserName.text?.count ?? 0) > 0 &&
+            (txtPassword.text?.count ?? 0) > 0
+        
+        if checkFormCharacter {
+            btnSignup.backgroundColor = UIColor.ConvertRgb(red: 20, green: 155, blue: 205)
+            btnSignup.isEnabled = true
+        }else{
+            btnSignup.backgroundColor = UIColor.ConvertRgb(red: 150, green: 205, blue: 245)
+            btnSignup.isEnabled = false
+        }
+    }
+    
     let txtUserName: UITextField = {
         let txt = UITextField()
-        txt.translatesAutoresizingMaskIntoConstraints = false
+        //        txt.translatesAutoresizingMaskIntoConstraints = false // it's the sam like above situation
         txt.placeholder = "Enter your username.."
         txt.backgroundColor = UIColor(white: 0, alpha: 0.05)
         txt.borderStyle = .roundedRect
         txt.font = UIFont.systemFont(ofSize: 16)
+        txt.addTarget(self, action: #selector(valueChanging), for: .editingChanged)
         return txt
     }()
     let txtPassword: UITextField = {
         let txt = UITextField()
-        txt.translatesAutoresizingMaskIntoConstraints = false
+        //        txt.translatesAutoresizingMaskIntoConstraints = false
         txt.isSecureTextEntry = true
         txt.placeholder = "Enter your password.."
         txt.backgroundColor = UIColor(white: 0, alpha: 0.05)
         txt.borderStyle = .roundedRect
         txt.font = UIFont.systemFont(ofSize: 16)
+        txt.addTarget(self, action: #selector(valueChanging), for: .editingChanged)
         return txt
     }()
     let btnSignup : UIButton = {
         let btn = UIButton(type: UIButton.ButtonType.system)
         btn.setTitle("Sign up", for: UIControl.State.normal)
-//        btn.backgroundColor = UIColor(red: 150/255, green: 205/255, blue: 245/255, alpha: 1)
+        //        btn.backgroundColor = UIColor(red: 150/255, green: 205/255, blue: 245/255, alpha: 1)
         // crated an extention for UIColor
         btn.backgroundColor = UIColor.ConvertRgb(red: 150, green: 205, blue: 245)
         btn.layer.cornerRadius = 6
         btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         btn.setTitleColor(.white, for: .normal)
+        btn.addTarget(self, action: #selector(btnSignupClicked), for: UIControl.Event.touchUpInside)
+        btn.isEnabled = false
         
         return btn
     }()
-
+    
+    @objc func btnSignupClicked(){
+        
+        guard let email = txtEmail.text else { return }
+        guard let password = txtPassword.text else { return }
+        guard let userName = txtUserName.text else { return }
+        
+        
+        //        let email = "test@gmail.com"
+        //        let userName = "user"
+        //        let password =  "123456"
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print("error \(error.localizedDescription)")
+                return
+            }
+            
+            print("user account created. id: \(result?.user.uid)")
+            self.txtEmail.text = ""
+            self.txtPassword.text = ""
+            self.txtUserName.text = ""
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -72,10 +118,12 @@ class ViewController: UIViewController {
         //        btnAddImage.center = view.center
         
         //we did it isActive because of we want to work the constraints which we'd given
-        btnAddImage.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        btnAddImage.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        //        btnAddImage.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        //        btnAddImage.heightAnchor.constraint(equalToConstant: 150).isActive = true
         btnAddImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        btnAddImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        //        btnAddImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        // we said that safeAreaLayoutGuide-> it's gonna take safe area frame before it was superview frame
+        btnAddImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, bottom: nil, leading: nil, trailing: nil, paddingTop: 40, paddingBottom: 0, paddingLeading: 0, paddingTrailing: 0, width: 150, height: 150)
         
         //we can write it also with this way without writing isActive=true everytime
         //        NSLayoutConstraint.activate([
@@ -94,31 +142,30 @@ class ViewController: UIViewController {
         //        let redView = UIView()
         //        redView.backgroundColor = .red
         
-        let blueView = UIView()
-        blueView.backgroundColor = .blue
+        //        let blueView = UIView()
+        //        blueView.backgroundColor = .blue
         
-        let stackView = UIStackView(arrangedSubviews: [txtEmail,txtUserName,txtPassword,btnSignup]) //,redView
-        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let stackView = UIStackView(arrangedSubviews: [txtEmail,txtUserName,txtPassword,btnSignup]) //[redView,blueView]
+        
         stackView.distribution = .fillEqually
         stackView.axis = .vertical
         stackView.spacing = 10
         
-        
         view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-//            stackView.topAnchor.constraint(equalTo: btnAddImage.bottomAnchor, constant: 20),
-//            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 45),
-//            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -45),
+            //            stackView.topAnchor.constraint(equalTo: btnAddImage.bottomAnchor, constant: 20),
+            //            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 45),
+            //            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -45),
             
-            stackView.heightAnchor.constraint(equalToConstant: 230)
+            //            stackView.heightAnchor.constraint(equalToConstant: 230)
             
-      
+            
         ])
         
         
-         stackView.anchor(top: btnAddImage.bottomAnchor, bottom: nil, leading: view.leadingAnchor, trailing: view.trailingAnchor, paddingTop: 20, paddingBottom: 0, paddingLeading: 45, paddingTrailing: -45),
-     
+        stackView.anchor(top: btnAddImage.bottomAnchor, bottom: nil, leading: view.leadingAnchor, trailing: view.trailingAnchor, paddingTop: 20, paddingBottom: 0, paddingLeading: 45, paddingTrailing: -45, width: 0, height: 230)
+        
     }
     
     
@@ -132,7 +179,10 @@ extension UIView {
                 paddingTop: CGFloat,
                 paddingBottom: CGFloat,
                 paddingLeading:CGFloat,
-                paddingTrailing:CGFloat){
+                paddingTrailing:CGFloat,
+                width:CGFloat,
+                height:CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
         if let top = top {
             self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
         }
@@ -145,7 +195,12 @@ extension UIView {
         if let trailing = trailing{
             self.trailingAnchor.constraint(equalTo: trailing, constant: paddingTrailing).isActive = true
         }
-   
+        if width != 0 {
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if height != 0 {
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
         
     }
 }
