fa851522db73e37dfd782a52adf23d76f9660026
Halil <halilibrahimbaykan@gmail.com>
2020-07-24 11:06:45 +0200

    add collectionView and header in the searhController

--- InstagramCloneProject/Add Media/PhotoSelectController.swift
+++ InstagramCloneProject/Add Media/PhotoSelectController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class PhotoSelectController: UICollectionViewController {
+    
+    let cellId = "cellId"
+    let headerId = "headerId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
        
         collectionView.backgroundColor = .yellow
         addButtons()
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        header.backgroundColor = .blue
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize { // it triggers to execute creating header
+        let width = view.frame.width
+        return CGSize(width: width, height: width)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets { // it uses for useally give some spaces inside of this item.
+        return UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .brown
+        return cell
     }
 
     override var prefersStatusBarHidden: Bool { // it doesn't work at first we need config navbar
@@ -24,9 +55,13 @@ class PhotoSelectController: UICollectionViewController {
     fileprivate func addButtons(){
         navigationController?.navigationBar.tintColor = .black
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelButtonClicked))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextButtonClicked))
     }
     @objc func cancelButtonClicked(){
         dismiss(animated: true, completion: nil) // when you press it, it dismiss current view immediately
+    }
+    @objc func nextButtonClicked(){
+        print("abc")
     }
 
 }
@@ -34,5 +69,18 @@ class PhotoSelectController: UICollectionViewController {
 extension UINavigationController{
     open override var childForStatusBarHidden: UIViewController?{
         return self.topViewController
+    }
+}
+
+extension PhotoSelectController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 3) / 4
+        return CGSize(width: width, height: width)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
     }
 }
